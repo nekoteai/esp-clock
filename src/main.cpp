0bd40e61e0106153ad026f6f411d35605c6a9a37
NEKT <87892508+nekoteai@users.noreply.github.com>
2022-07-04 16:20:34 +0800

    working weather api and display

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,4 @@
 #include "Arduino.h"
-#include "EEPROM.h"
 #include "FS.h"
 #include "Wire.h"
 #include "U8g2lib.h"
@@ -7,64 +6,24 @@
 #include "time.h"
 #include "WiFi.h"
 #include "sntp.h"
-char buff[200];
-char inbyte;
-int i;
+#include "Preferences.h"
+Preferences preferences;
+const char *loc_timezone="CST-8";
+const char *ssid = "COIO";
+const char *password = "87654321";
+//Weather Info
+char Allinfo[128];
+char zone[64];
+short int temper;
+char DayWeather[32];
+char windir[32];
+short int windpower;
+short int humidity;
 time_t now;
 char strftime_buf[64];
 struct tm timeinfo;
-void connman();
-U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0,21,22,U8X8_PIN_NONE);
-void printLocalTime()
-{
-  if(!getLocalTime(&timeinfo)){
-    Serial.println("No time available (yet)");
-    return;
-  }
-  Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S");
-}
-void timeavailable(struct timeval *t)
-{
-  Serial.println("Got time adjustment from NTP!");
-  printLocalTime();
-}
+U8G2_SSD1306_128X64_NONAME_F_SW_I2C u8g2(U8G2_R0,21,22,U8X8_PIN_NONE);
 
-void setup(void){
-    Serial.begin(115200);    
-    while(!Serial){
-        ;
-    }
-    
-    pinMode(21,OUTPUT);
-    pinMode(22,OUTPUT);
-    digitalWrite(21, 0);
-    digitalWrite(22, 0);
-    //sntp_set_time_sync_notification_cb(Serial.println("TIME AVAILA"));
-    sntp_set_time_sync_notification_cb( timeavailable );
-    sntp_servermode_dhcp(1); 
-    configTzTime("CST-8","223.113.103.191","203.107.6.88 ",0);
-    connman();
-    u8g2.begin();
-    u8g2.setFont(u8g2_font_wqy16_t_chinese1);
-    u8g2.setFontDirection(0);
-    Serial.println("Wire Error");
-    //set_clock(loc_timezone);
-    //localtime_r(&now, &timeinfo);
-    
-
-}
-void loop(void){
-//weatherinfo();
-  //printLocalTime();
-  getLocalTime(&timeinfo);
-  strftime(strftime_buf, sizeof(strftime_buf), "%T", &timeinfo);
-  u8g2.clearBuffer();
-  u8g2.drawFrame(32,16,48,24);
-    u8g2.setCursor(48,24);
-    u8g2.print(strftime_buf);
-  u8g2.sendBuffer();
-  delay(500);
-}
 void connman(){
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
@@ -72,4 +31,54 @@ void connman(){
       Serial.print(".");
   }
     Serial.println(" CONNECTED");
+}
+bool WeatherDisplay(short int dmode){
+  if (dmode==0){
+    u8g2.firstPage();
+    do{
+      u8g2.drawHLine(0,34,129);
+      u8g2.setCursor(16,24);
+      u8g2.setFont(u8g2_font_ncenB18_tn);
+      u8g2.print(strftime_buf);
+      u8g2.enableUTF8Print();
+      u8g2.setFont(u8g2_font_wqy13_t_gb2312);
+      u8g2.setCursor(16,48);
+      u8g2.print(Allinfo);
+      //u8g2.setCursor(54,48);
+      //u8g2.print(DayWeather);
+
+    }while(u8g2.nextPage());
+  }
+  return 1;
+}
+void setup(void){
+    serialinit(115200);//initial serial
+    preferences.begin("clock", false);
+    preferences.putString("citycode", "440300");
+    String citycode = preferences.getString("citycode","0");
+    preferences.end();
+    pinsetup();
+
+    sntp_set_time_sync_notification_cb( timeavailable );
+    sntp_servermode_dhcp(1); 
+    configTzTime("CST-8","223.113.103.191",0,0);
+
+    connman();
+    delay(1000);
+    u8g2.begin();
+    u8g2.setFontDirection(0);
+    
+    //set_clock(loc_timezone);
+    //localtime_r(&now, &timeinfo);
+    getDayWeather(zone,&temper,DayWeather,windir,&windpower,&humidity,citycode);
+    sprintf(Allinfo,"%s %d℃%s %s风%d级湿度%d%%",zone,temper,DayWeather,windir,windpower,humidity);
+
+
+}
+void loop(void){
+  getLocalTime(&timeinfo);
+  strftime(strftime_buf, sizeof(strftime_buf), "%T", &timeinfo);
+  WeatherDisplay(0);
+  			
+  delay(500);
 }
--- src/other.cpp
+++ src/other.cpp
@@ -1,5 +1,9 @@
 #include "Arduino.h"
 #include "time.h"
+#include "Preferences.h"
+extern time_t now;
+extern char strftime_buf[64];
+extern struct tm timeinfo;
 void set_clock(const char *tz){
     Serial.print(F("Waiting for NTP time sync: "));
     time_t nowSecs = time(nullptr);
@@ -10,4 +14,33 @@ void set_clock(const char *tz){
         nowSecs = time(nullptr);
     }
     Serial.print(nowSecs);
+}
+
+bool serialinit(int rate){
+    Serial.begin(rate);    
+    while(!Serial){
+        ;
+    }
+    
+    return 1;
+}
+
+void pinsetup(){
+    pinMode(21,OUTPUT);
+    pinMode(22,OUTPUT);
+    digitalWrite(21, 0);
+    digitalWrite(22, 0);
+}
+void printLocalTime()
+{
+  if(!getLocalTime(&timeinfo)){
+    Serial.println(F("No time available (yet)"));
+    return;
+  }
+  //Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S");
+}
+void timeavailable(struct timeval *t)
+{
+  Serial.println(F("Got time adjustment from NTP!"));
+  printLocalTime();
 }
